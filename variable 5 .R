c3c44b000fd88d39514d2e565e74883498a1dd6f
kglove11 <156819984+kglove11@users.noreply.github.com>
2024-01-30 10:14:10 -0500

    Add files via upload

---
+++ variable 5 .R
@@ -0,0 +1,20 @@
+Author: Glover Kayla  date 01/30/24
+Purpose in class assignment :
+Create  variable calle d variable 5 with the numbers: 5,10,90,340,890, 999999999, 10000000000001
+
+
+
+variable5<-c(5,10,90,340,890,999999999,100000000001)
+mean(variable5)
+mean(variable5)
+[1] 14428571619
+> sd(variable5)
+[1] 37735293487
+> 
+> median(variable5)
+[1] 340
+> mode = function() {return(names(sort(-table(variable5)))[1]) }
+> mode()
+[1] "5"
+
+ 
