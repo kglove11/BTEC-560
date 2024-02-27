751c94a47e337985b13f0920e2528fc44516892a
kglove11 <156819984+kglove11@users.noreply.github.com>
2024-02-27 10:40:31 -0500

    Add files via upload

---
+++ 2:27:24 pawar .R
@@ -0,0 +1,20 @@
+#Kayla Glover date 02/27/2024, Purpose : Use ANOVA on sample data set 
+
+#load library 
+library(dplyr)
+
+#load sample data set 
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+df<-read.csv(PATH)
+
+#apply ANOVA TEST 
+anova_one_way <-aov(time~poison,data=df)
+summary(anova_one_way)
+
+Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+> 
