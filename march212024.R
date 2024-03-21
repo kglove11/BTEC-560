6d88ee325366c6a843bbf2bfa5bfae699dffcc89
kglove11 <156819984+kglove11@users.noreply.github.com>
2024-03-21 10:14:55 -0400

    Add files via upload

---
+++ march212024.R
@@ -0,0 +1,18 @@
+Author Kayla Glover date march 21, 2024
+
+x=rnorm(10)
+y=rnorm(10)
+tttest=t.test(x,y)
+
+> ttest
+
+	Welch Two Sample t-test
+
+data:  x and y
+t = 0.33222, df = 17.715, p-value = 0.7436
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.8123424  1.1170929
+sample estimates:
+ mean of x  mean of y 
+0.16595944 0.01358423 
